bf15852980dc83c9a9b13ee5ec3e80fb77977126
Mumin Asaad <mumin.sedco@outlook.com>
2018-10-02 15:48:45 +0300

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import '../libraries/pdfobject';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('pdfLocation') pdfLocationDiv: ElementRef;
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+
+    getWindow().PDFObject.embed('/assets/Resume_Vol1.pdf', '#pdfLocation');
+
+  }
+
+
+
+}
+
+
+function getWindow(): any {
+  return window;
 }
